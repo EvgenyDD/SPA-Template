9e816a33830968f1125d8f981fbf195d05ca6405
Eugene <evgdol93@gmail.com>
2022-02-05 22:58:03 +0300

    [*]

---
+++ Трюки CSS и JavaScript, которые вдохнут жизнь в ваш статический сайт _ Блог компании SkillFactory _ Хабр_files/page_data/xR9QzUGdQbV.js
@@ -0,0 +1,3 @@
+if (self.CavalryLogger) { CavalryLogger.start_js(["z2771nb"]); }
+
+__d("TransAppInlineMode",[],(function(a,b,c,d,e,f){a=Object.freeze({STRING_MANAGER:"STRING_MANAGER",TRANSLATION:"TRANSLATION",APPROVE:"APPROVE",REPORT:"REPORT",NO_INLINE:"NO_INLINE"});e.exports=a}),null);
